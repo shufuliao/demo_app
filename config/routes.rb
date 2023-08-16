2d7a8c8edee1bb5341d74f693886d6f104d01c43
ShuFu <liaovincent7801@gmail.com>
2023-08-16 22:47:18 +0800

    add route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :topics
-  root "topics#index"
+  root "topics#index" 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
